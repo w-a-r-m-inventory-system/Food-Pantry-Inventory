884eb14e8b62bbf6410fdbbdec26bf38f615d30d
Travis Risner <deeppunster@gmail.com>
2019-03-24 03:47:01 -0400

    Refined admin screens and added an omitted field.
    - Added __str__ functions to models so they display nicely on the
    admin screens.
    - Added duration to Activity table for ease of reporting later.
    - Added models to admin.py so administrators can view/edit the tables.

--- fpiweb/admin.py
+++ fpiweb/admin.py
@@ -1,3 +1,18 @@
+"""
+Admin.py - Identify what can be mangaged by administrators.
+"""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import BoxType, Box, Activity, Product, ProductCategory, \
+    Constraints
+
+# Register the models for which we want default admin pages to be built.
+admin.site.register(Box)
+admin.site.register(BoxType)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(Activity)
+admin.site.register(Constraints)
+
+# EOF
--- fpiweb/models.py
+++ fpiweb/models.py
@@ -15,6 +15,12 @@ class BoxType(models.Model):
     BoxTypeDescr = models.CharField('Box Type Description', max_length=30)
     BoxTypeQty = models.IntegerField('Default Box Type Quantity')
 
+    # define a default display of BoxType
+    def __str__(self):
+        display = f'{self.BoxTypeCode} - {self.BoxTypeDescr} ' \
+            f'({self.BoxTypeQty})'
+        return display
+
 
 class ProductCategory(models.Model):
     """
@@ -24,6 +30,11 @@ class ProductCategory(models.Model):
                                  primary_key=True)
     ProdCatName = models.CharField('Product Category Name',
                                    max_length=30, unique=True)
+
+    # define a default display of Product Category
+    def __str__(self):
+        display = f'{self.ProdCatName}'
+        return display
 
 
 class Product(models.Model):
@@ -35,6 +46,11 @@ class Product(models.Model):
     ProdCat = models.ForeignKey(ProductCategory, on_delete=models.PROTECT,
                                 verbose_name='Product Category')
 
+    # define a default display of Product
+    def __str__(self):
+        display = f'{self.ProdName} ({self.ProdCat})'
+        return display
+
 
 class Box(models.Model):
     """
@@ -45,18 +61,40 @@ class Box(models.Model):
                                  unique=True)
     BoxType = models.ForeignKey(BoxType, on_delete=models.PROTECT,
                                 verbose_name='Type of Box')
-    LocRow = models.CharField('Row Location', max_length=2, null=True)
-    LocBin = models.CharField('Bin Location', max_length=2, null=True)
-    LocTier = models.CharField('Tier Location', max_length=2, null=True)
+    LocRow = models.CharField('Row Location', max_length=2, null=True,
+                              blank=True)
+    LocBin = models.CharField('Bin Location', max_length=2, null=True,
+                              blank=True)
+    LocTier = models.CharField('Tier Location', max_length=2, null=True,
+                               blank=True)
     Product = models.ForeignKey(Product, on_delete=models.PROTECT,
-                                verbose_name='Product', null=True)
-    ExpirationYear = models.IntegerField('Year Product Expires', null=True)
+                                verbose_name='Product', null=True, blank=True)
+    ExpirationYear = models.IntegerField('Year Product Expires', null=True,
+                                         blank=True)
     ExpirationMonthStart = models.IntegerField('Expiration Start Month '
-                                               '(Optional)', null=True)
+                                               '(Optional)', null=True,
+                                               blank=True)
     ExpirationMonthEnd = models.IntegerField('Expiration End Month '
-                                             '(Optional)', null=True)
-    DateFilled = models.DateTimeField('Date Box Filled', null=True)
-    Quantity = models.IntegerField('Quantity in Box', null=True)
+                                             '(Optional)', null=True,
+                                             blank=True)
+    DateFilled = models.DateTimeField('Date Box Filled', null=True, blank=True)
+    Quantity = models.IntegerField('Quantity in Box', null=True, blank=True)
+
+    # define a default display of Box
+    def __str__(self):
+        if self.ExpirationMonthStart or self.ExpirationMonthEnd:
+            display = f'{self.BoxNumber} ({self.BoxType}) ' \
+                f'{self.LocRow}/{self.LocBin}/{self.LocTier} ' \
+                f'{self.Product} {self.Quantity}' \
+                f'{self.ExpirationYear} ' \
+                f'({self.ExpirationMonthStart}-{self.ExpirationMonthEnd})' \
+                f'{self.DateFilled}'
+        else:
+            display = f'{self.BoxNumber} ({self.BoxType}) ' \
+                f'{self.LocRow}/{self.LocBin}/{self.LocTier} ' \
+                f'{self.Product} {self.Quantity}' \
+                f'{self.ExpirationYear} {self.DateFilled}'
+        return display
 
 
 class Activity(models.Model):
@@ -71,34 +109,66 @@ class Activity(models.Model):
     ProdCatName = models.CharField('Product Category Name', max_length=30)
     DateFilled = models.DateField('Date Box Filled')
     DateConsumed = models.DateField('Date Box Emptied')
+    Duration = models.IntegerField('Duration')
     ExpirationYear = models.IntegerField('Year Expired')
     ExpirationMonthStart = models.IntegerField('Start Expiration Month',
-                                               null=True)
+                                               null=True, blank=True)
     ExpirationMonthEnd = models.IntegerField('End Expiration Month',
-                                             null=True)
+                                             null=True, blank=True)
     Quantity = models.IntegerField('Quantity in Box', null=True)
 
-
-CONSTRAINT_TYPE_CHOICES = (
-    ('Int-MM', 'Integer Min/Max'),
-    ('Char-MM', 'Character Min/Max'),
-    ('Int-List', 'Integer Valid List'),
-    ('Char-List', 'Character Valid List'),
-)
+    # define a default display of Activity
+    def __str__(self):
+        if self.DateFilled:
+            display = f'{self.BoxNumber} ({self.BoxTypeCode}) ' \
+                f'{self.ProdName} ({self.ProdCatName})' \
+                f'{self.Quantity} ' \
+                f'{self.ExpirationYear}' \
+                f'({self.ExpirationMonthStart}-{self.ExpirationMonthEnd})' \
+                f'{self.DateFilled} - {self.DateConsumed} ' \
+                f'({self.Duration})' \
+                f'at {self.LocRow}/{self.LocBin}/{self.LocTier} '
+        else:
+           display = f'{self.BoxNumber} ({self.BoxTypeCode}) - Empty'
+        return display
 
 class Constraints (models.Model):
     """
     Constraints of valid values.
     """
+
+    # Constraint Choice Names
+    INT_RANGE = 'Int-MM'
+    CHAR_RANGE = 'Char-MM'
+    INT_LIST = 'Int-List'
+    CHAR_LIST = 'Char-List'
+
+    CONSTRAINT_TYPE_CHOICES = (
+        (INT_RANGE, 'Integer Min/Max'),
+        (CHAR_RANGE, 'Character Min/Max'),
+        (INT_LIST, 'Integer Valid List'),
+        (CHAR_LIST, 'Character Valid List'),
+    )
+
     ConstraintID = models.AutoField('Internal Constraint ID', primary_key=True)
     ConstraintName = models.CharField('Constraint Name', max_length=30)
     ConstraintType = models.CharField('Constraint Type', max_length=15,
                                       choices=CONSTRAINT_TYPE_CHOICES)
     ConstraintMin = models.CharField('Minimum Valid Constraint', null=True,
-                                     max_length=30)
+                                     max_length=30, blank=True)
     ConstraintMax = models.CharField('Maximum Valid Constraint', null=True,
-                                     max_length=30)
+                                     max_length=30, blank=True)
     ConstraintList = models.CharField('Valid Constraint List', null=True,
-                                      max_length=500)
+                                      max_length=500, blank=True)
+
+    # define a default display of Constraints
+    def __str__(self):
+        if self.ConstraintType in [self.INT_RANGE, self.CHAR_RANGE]:
+            display = f'{self.ConstraintName} - {self.ConstraintMin} to ' \
+                f'{self.ConstraintMax} ({self.ConstraintType})'
+        else:
+            display = f'{self.ConstraintName} - {self.ConstraintList} ' \
+                f'({self.ConstraintType})'
+        return display
 
 # EOF
