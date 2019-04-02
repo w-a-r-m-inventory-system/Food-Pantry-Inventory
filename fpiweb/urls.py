8c03202987ac6e50617f7e066baf3ed042ec0355
John Cassidy <jocassid@gmail.com>
2019-04-02 01:01:35 -0400

    created login view

--- fpiweb/urls.py
+++ fpiweb/urls.py
@@ -7,5 +7,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
--- fpiweb/views.py
+++ fpiweb/views.py
@@ -1,10 +1,15 @@
 """
 views.py - establish the views (pages) for the F. P. I. web application.
 """
-from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+
+from fpiweb.forms import LoginForm
+
 
 def index(request):
     """
@@ -16,5 +21,30 @@ def index(request):
 
     response = HttpResponse("Hello world from Food Pantry Inventory.")
     return response
+
+
+class LoginView(FormView):
+    template_name = 'fpiweb/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password
+        )
+
+        if user is None:
+            form.add_error(None, "Invalid username and/or password")
+            return self.form_invalid(form)
+
+        login(self.request, user)
+        return super().form_valid(form)
+
+
 
 # EOF
