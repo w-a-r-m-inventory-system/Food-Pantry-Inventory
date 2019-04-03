3dd90b17574db808983e60fa3073a2bc185d15ed
Travis Risner <deeppunster@gmail.com>
2019-04-03 01:10:01 -0400

    Added support for list/add/change/delete of constraints.

--- fpiweb/urls.py
+++ fpiweb/urls.py
@@ -6,7 +6,9 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 
-from . import views
+from fpiweb.views import AboutView, ConstraintsListView, index, \
+    ConstraintDetailView, ConstraintCreateView, ConstraintUpdateView, \
+    ConstraintDeleteView
 
 # set the namespace for the application
 app_name = 'fpiweb'
@@ -14,15 +16,35 @@ app_name = 'fpiweb'
 urlpatterns = [
 
     # index page
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('index/', index, name='index'),
 
     # about page
-    path('about/', views.AboutView.as_view()),
+    path('about/', AboutView.as_view, name='about_view'),
     # path('about/', views.AboutView.as_view(template_name='about.html')),
-
-    # index page
-    path('index/', views.index, name='index'),
 
     # about page
     # path('about/', views.AboutView, name='about'),
+
+    # Constraint List page
+    # e.g. /fpiweb/constraints/ = list of constraints
+    path('constraints/', ConstraintsListView.as_view(),
+         name='constraints_view'),
+
+    # e.g. /fpiweb/constraints/4/ = show constraint # 4
+    path('constraint/<int:constraint>', ConstraintDetailView.as_view(),
+         name='constraint_detail', ),
+
+    # e.g. /fpiweb/constraints/add/ = add a constraint
+    path('constraint/add/', ConstraintCreateView.as_view(),
+      name='constraint_new', ),
+
+    # e.g. /fpiweb/constraints/edit/4/ = edit constraint # 4
+    path('constraint/edit/<int:constraint>', ConstraintUpdateView.as_view(),
+        name='constraint_update', ),
+
+    # # e.g. /fpiweb/constraints/delete/4/ = delete constraint # 4
+    path('constraint/delete/<int:constraint>', ConstraintDeleteView.as_view(),
+        name='constraint_delete', ),
+
 ]
