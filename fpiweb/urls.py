f21e1451fc2855bae479fe3e29ffb28d40cc4669
Travis Risner <deeppunster@gmail.com>
2019-05-11 14:24:28 -0400

    Tweakboxadd (#49)
    * Logout support - W.I.P.
    
    * Added logout screen and logic.  Also tweaked the Git Guidelines.
    
    - Added bare bones logout screen and logic.
    -- Still needs more tweaks (use base.html, etc.)
    - Added PyCharm plugin for Database Navigator that was overlooked.
    
    * Attempt to add polish to John C. improvements.
    
    * Update some libraries including avoidinga vulnerability.
    
    * Added black and its dependencies.
    
    * Restored reference to private_settings from private directory.
    
    * Minor tweaks that incorporate John Cassidy's suggestions.
    
    John's latest pull request indicated that URL named references should
    include a trailing slash in  the nsmes.  I have made these changes
    so the code is consistant.

--- fpiweb/urls.py
+++ fpiweb/urls.py
@@ -23,25 +23,25 @@ urlpatterns = [
 
     # index page
     # e.g. /fpiweb/ or /fpiweb/index/
-    path('', IndexView.as_view(), name='index'),
-    path('index/', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index/'),
+    path('index/', IndexView.as_view(), name='index/'),
 
     # about page
     # e.g. /fpiweb/about/
-    path('about/', AboutView.as_view(), name='about'),
+    path('about/', AboutView.as_view(), name='about/'),
 
     # login page
     # e.g. /fpiweb/login/
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login/'),
 
     # logout page
     # e.g. /fpiweb/logout/
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout/'),
 
     # Constraint List page
     # e.g. /fpiweb/constraints/ = list of constraints
     path('constraints/', ConstraintsListView.as_view(),
-         name='constraints_view'),
+         name='constraints_view/'),
 
     # # e.g. /fpiweb/constraints/4/ = show constraint # 4
     # path('constraint/<int:constraint>', ConstraintDetailView.as_view(),
@@ -49,15 +49,16 @@ urlpatterns = [
 
     # e.g. /fpiweb/constraints/add/ = add a constraint
     path('constraint/add/', ConstraintCreateView.as_view(),
-      name='constraint_new', ),
+      name='constraint_new/', ),
 
     # e.g. /fpiweb/constraints/edit/4/ = edit constraint # 4
     path('constraint/edit/<int:pk>', ConstraintUpdateView.as_view(),
-        name='constraint_update', ),
+        name='constraint_update/', ),
 
-    # # e.g. /fpiweb/constraints/delete/4/ = delete constraint # 4
+    # e.g. /fpiweb/constraints/delete/4/ = delete constraint # 4
     path('constraint/delete/<int:pk>', ConstraintDeleteView.as_view(),
-        name='constraint_delete', ),
+        name='constraint_delete/', ),
 
-    path('box/add/', BoxAddView.as_view(), name='box_add')
+    # e.g.  /fpiweb/box/add/ = add a box to inventory
+    path('box/add/', BoxAddView.as_view(), name='box_add/'),
 ]
