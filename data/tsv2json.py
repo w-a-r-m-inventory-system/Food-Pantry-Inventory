cce4ae37df14426b302bb1bd5333b82d1fc24fbc
John Cassidy <jocassid@gmail.com>
2019-05-21 15:56:54 -0400

    Add inventory screen validations (#60)
    * added blurb on how to generate secret key
    
    * pulled slashes from path name.  Path names allow you to change URL path without having to modify code which references the name.  Got BoxAddView to validate with blank expires month
    
    * added additional fields and cross-field validation
    
    * validation of expiration month fields

--- data/tsv2json.py
+++ data/tsv2json.py
@@ -1,22 +1,24 @@
 
 from argparse import ArgumentParser
 from json import dumps
-from sys import stdout
+from sys import stderr
 
 
 MODEL_DEFS = {
     'fpiweb.productcategory': [
         ('prod_cat_name', str),
         ('prod_cat_descr', str)
-    ]
+    ],
+    'fpiweb.product': [
+        ('prod_name', str),
+        ('prod_cat', int),
+    ],
 }
 
 
-
-
-
-
 def main(args):
+
+    field_defs = MODEL_DEFS.get(args.model)
 
     records = []
     with open(args.IN_FILE, 'r') as in_file:
@@ -27,12 +29,21 @@ def main(args):
 
             pk, *field_values = line.split('\t')
 
+            if len(field_defs) != len(field_values):
+                print(f"Wrong number of fields in line {i}", file=stderr)
+                continue
 
+            mappings = zip(field_values, field_defs)
+            mappings = [(fv, fd[1], fd[0]) for fv, fd in mappings]
+
+            fields = {}
+            for value, converter, dest in mappings:
+                fields[dest] = converter(value)
 
             data = {
                 'model': args.model,
                 'pk': int(pk),
-                'fields': [],
+                'fields': fields,
             }
 
             records.append(data)
--- fpiweb/forms.py
+++ fpiweb/forms.py
@@ -5,14 +5,11 @@ forms.py - provide validation of a forms.
 from logging import getLogger, debug, error
 
 from django import forms
-from django.core.exceptions import ValidationError
-from django.forms import inlineformset_factory, BaseInlineFormSet
-from django.forms import CharField, Form, PasswordInput, ValidationError
+from django.forms import CharField, DateInput, Form, PasswordInput, ValidationError
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
 from fpiweb.models import Box, BoxType, Constraints, Product, ProductCategory
-
 
 
 __author__ = '(Multiple)'
@@ -21,7 +18,9 @@ __creation_date__ = "04/01/2019"
 # "${CopyRight.py}"
 
 
-month_choices = [('--', '--')] + [(str(i), str(i)) for i in range(1, 13)]
+month_choices = [(None, '--')] + [(str(i), str(i)) for i in range(1, 13)]
+
+# logger = getLogger(__name__)
 
 
 def expire_year_choices():
@@ -42,6 +41,7 @@ def char_list_choices(constraint_name):
     values = Constraints.get_values(constraint_name)
     return list(zip(values, values))
 
+
 def row_choices():
     return min_max_choices('Row')
 
@@ -54,9 +54,14 @@ def tier_choices():
     return char_list_choices('Tier')
 
 
+def none_or_int(text):
+    if text is None or text.strip() == '':
+        return None
+    return int(text)
 
 
-# log = getLogger(__name__)
+class Html5DateInput(DateInput):
+    input_type = 'date'
 
 
 class LogoutForm(Form):
@@ -94,8 +99,19 @@ class BoxForm(forms.ModelForm):
         fields = [
             'box_number',
             'box_type',
+            'loc_row',
+            'loc_bin',
+            'loc_tier',
             'product',
+            'exp_year',
+            'exp_month_start',
+            'exp_month_end',
+            'date_filled',
+            'quantity'
         ]
+        widgets = {
+            'date_filled': Html5DateInput
+        }
 
     loc_row = forms.ChoiceField(
         choices=row_choices,
@@ -120,14 +136,44 @@ class BoxForm(forms.ModelForm):
 
     exp_month_start = forms.TypedChoiceField(
         choices=month_choices,
-        coerce=int,
+        required=False,
+        empty_value=None,
+        coerce=none_or_int,
         help_text=Box.exp_month_start_help_text,
     )
 
     exp_month_end = forms.TypedChoiceField(
         choices=month_choices,
-        coerce=int,
+        required=False,
+        empty_value=None,
+        coerce=none_or_int,
         help_text=Box.exp_month_end_help_text,
     )
+
+    @staticmethod
+    def validate_exp_month_start_end(exp_month_start, exp_month_end):
+        if exp_month_start is None and exp_month_end is None:
+            return
+
+        error_msg = "If Exp {} month is specified, Exp {} month must be specified"
+
+        if exp_month_start is not None and exp_month_end is None:
+            raise ValidationError(error_msg.format('start', 'end'))
+
+        if exp_month_end is not None and exp_month_start is None:
+            raise ValidationError(error_msg.format('end', 'start'))
+
+        if exp_month_end <= exp_month_start:
+            raise ValidationError('Exp month end must be after Exp month start')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        print("cleaned_data", cleaned_data)
+
+        exp_month_start = cleaned_data.get('exp_month_start')
+        exp_month_end = cleaned_data.get('exp_month_end')
+        self.validate_exp_month_start_end(exp_month_start, exp_month_end)
+
+
 
 # EOF
