eeabaf54782f422087f4b1cea1ab6a1dbd3f387d
John Cassidy <jocassid@gmail.com>
2019-06-26 02:24:01 -0400

    Merge test scan (#78)
    * Moved class Meta: blocks to top of model classes.  Added verbose_name_plural.  Added list_filter to BoxAdmin.
    
    * Created test_scan page.
    
    * added template test_scan.html
    
    * scan QR code
    
    * added qrcode dependency
    
    * removed quantity fro BoxForm, setting Box.quantity to BoxType.box_type_qty
    
    * renamed box add view, form, url, etc. to new box
    
    * rename of template
    
    * switch from add box to new box.  Split original edit box form into parts
    
    * Test scan enhancements (#74) (#76)
    
    * Minor fixes to make test_scan work and update requirements.
    
    * pared down new box form to just box type (#75)
    
    * Moved class Meta: blocks to top of model classes.  Added verbose_name_plural.  Added list_filter to BoxAdmin.
    
    * Created test_scan page.
    
    * added template test_scan.html
    
    * scan QR code
    
    * work in progress
    
    * added qrcode dependency
    
    * removed quantity fro BoxForm, setting Box.quantity to BoxType.box_type_qty
    
    * renamed box add view, form, url, etc. to new box
    
    * rename of template
    
    * switch from add box to new box.  Split original edit box form into parts
    
    * mostly changes due to breaking up box form

--- fpiweb/urls.py
+++ fpiweb/urls.py
@@ -9,11 +9,14 @@ from django.views.generic import TemplateView
 from fpiweb.views import \
     AboutView, \
     BoxEditView, \
+    BoxEmptyView, \
     BoxEmptyMoveView, \
+    BoxFillView, \
+    BoxMoveView, \
     BoxScannedView, \
     IndexView, LoginView, ConstraintsListView, \
     ConstraintCreateView, ConstraintUpdateView, ConstraintDeleteView, \
-    LogoutView, BoxAddView, BoxDetailsView, \
+    LogoutView, BoxNewView, BoxDetailsView, \
     TestScanView
 
 # from fpiweb.views import ConstraintDetailView
@@ -55,7 +58,7 @@ urlpatterns = [
 
     # e.g. /fpiweb/constraints/add/ = add a constraint
     path('constraint/add/', ConstraintCreateView.as_view(),
-      name='constraint_new', ),
+        name='constraint_new', ),
 
     # e.g. /fpiweb/constraints/edit/4/ = edit constraint # 4
     path('constraint/edit/<int:pk>/', ConstraintUpdateView.as_view(),
@@ -66,7 +69,7 @@ urlpatterns = [
         name='constraint_delete', ),
 
     # e.g.  /fpiweb/box/add/ = add a box to inventory
-    path('box/add/', BoxAddView.as_view(), name='box_add'),
+    path('box/new/<str:box_number>/', BoxNewView.as_view(), name='box_new'),
 
     # e.g. /fpiweb/box/<pk>/edit = edit a box in inventory
     path('box/<int:pk>/edit/', BoxEditView.as_view(), name='box_edit'),
@@ -80,6 +83,22 @@ urlpatterns = [
     # e.g. /fpiweb/box/<pk>/empty_move = consume or move a box
     path('box/<int:pk>/empty_move/', BoxEmptyMoveView.as_view(),
          name='box_empty_move'),
+
+    # e.g. /fpiweb/test_scan/ = ???
+    path('box/<int:pk>/move/', BoxMoveView.as_view(), name='box_move'),
+
+    path('box/<int:pk>/fill/', BoxFillView.as_view(), name='box_fill'),
+
+    path('box/<int:pk>/empty/', BoxEmptyView.as_view(), name='box_empty'),
+
+    # e.g. /fpiweb/box/<pk>/move/ = change location of box in inventory
+    path('box/<int:pk>/move/', BoxEmptyMoveView.as_view(), name='box_move'),
+
+    # e.g. /fpiweb/box/<pk>/fill/ = fill an empy box and put in inventory
+    path('box/<int:pk>/fill/', BoxEmptyMoveView.as_view(), name='box_fill'),
+
+    # e.g. /fpiweb/box/<pk>/empty = consume the product in a box
+    path('box/<int:pk>/empty/', BoxEmptyMoveView.as_view(), name='box_empty'),
 
     # e.g. /fpiweb/test_scan/ = ???
     path('test_scan/', TestScanView.as_view(), name='test_scan'),
