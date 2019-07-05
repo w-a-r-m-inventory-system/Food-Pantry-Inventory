3a4c81fde5c6ad825b60b3d63ca3724fd7ea8412
Travis Risner <deeppunster@protonmail.com>
2019-07-05 01:07:55 -0400

    Load location data (#79)
    * Added a standalone program, data, template fixes, and updated reg.txt.
    
    - Added a standalone program to populate the new Row, Bin, Tier and
      Location tables.
    - Added data for those four tables to the fixtures directory.
    -- They can just be loaded instead of running the program.
    -- Provides data for testing.
    - Modified the constraint_delete.html so it works again.
    - Modified the FPIDjango/urls.py file so the fpiweb suffix to the
      starting URL is no longer required.
    - Updated the requirements.txt file to urge loading the latest version
      of Django because the previous version was considered vulnerable.

--- FPIDjango/urls.py
+++ FPIDjango/urls.py
@@ -2,18 +2,13 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 Examples:
 Function views
-
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-
 Class-based views
-
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -21,19 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from fpiweb.views import \
-    IndexView, \
-    LoginView
-
+from fpiweb.views import IndexView
 
 __author__ = '(Multiple)'
 __project__ = "Food-Pantry-Inventory"
 __creation_date__ = "04/01/2019"
 
 urlpatterns = [
-    path('', LoginView.as_view()),
     path('fpiweb/', include('fpiweb.urls')),
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view())
 ]
 
 # EOF
