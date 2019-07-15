85e9f0dbf841d66b379bca86b03d5c7c6bc51722
Travis Risner <deeppunster@protonmail.com>
2019-07-15 03:29:33 -0400

    Update Dev to include Constraint Edit and Delete (#83)
    * Adjust code so Constraint Edit and Delete work properly.
    
    - Also revised UML to include pallet handling.

--- FPIDjango/urls.py
+++ FPIDjango/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from fpiweb.views import \
-    IndexView, \
+from fpiweb.views import IndexView, \
     LoginView
 
 
@@ -26,7 +25,6 @@ __project__ = "Food-Pantry-Inventory"
 __creation_date__ = "04/01/2019"
 
 urlpatterns = [
-    path('', LoginView.as_view()),
     path('fpiweb/', include('fpiweb.urls')),
     path('admin/', admin.site.urls),
     path('', IndexView.as_view())
