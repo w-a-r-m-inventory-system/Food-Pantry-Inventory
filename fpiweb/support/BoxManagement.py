1dbd98d5924aa4609eff97165406652ec96a6b37
Travis Risner <deeppunster@fastmail.com>
2020-01-20 17:27:44 -0500

    feat: Box management documentation and stubs (#147)
    - Documents box management and implements stubs to call.
    - Stubs are not
    functional; box records are not updated.

--- fpiweb/support/BoxManagement.py
+++ fpiweb/support/BoxManagement.py
@@ -33,7 +33,23 @@ class BoxManagementClass:
 
     def box_new(self, box_number: str, box_type: BoxType) -> Box:
         """
-        Add a new (empty box) to inventory.
+        Add a new empty box to the inventory system.  If successful it
+        adds an activity record for an empty box and returns the box record
+        just created.
+
+        Requirements:
+
+        *   Box number is valid, unique, and not prevously assigned
+
+        *   Box type is valid
+
+        Exceptions:
+
+            xxx - Box number supplied is not in the valid format ('BOXnnnn')
+
+            xxx - Box number is not unique
+
+            xxx - Box type is not valid
 
         :param box_number: in the form of 'BOXnnnnn'
         :param box_type: a valid box type
@@ -49,13 +65,29 @@ class BoxManagementClass:
             product: Union[Product, int], exp_year: int,
             exp_mo_start: int = 0, exp_mo_end: int = 0):
         """
-        Fill an individual box with product and add to the inventory.
+        Fill an individual box with product and add to the inventory.  If
+        the box is not empty, an activity record will empty the box of its
+        previous contents and a new activity record will note the new
+        contents profiled.  If successful, it will return the box just
+        filled.
 
         Requirements:
+
         *   Box record has not been modified
-        *   All required fields be valid
-        *   Optional month start and end, if specified, must bracket one or
-        more months
+
+        *   All required fields are valid
+
+        *   Optional month start and end, if specified, bracket one or
+            more months
+
+        Exceptions:
+
+            xxx - location supplied is not valid
+
+            xxx - the product supplied is not valid
+
+            xxx - the expiration  year, start month, and/or end month are
+                not valid or are out of range
 
         :param box: Box record or id of a box already in the system
         :param location: Target location record or ID
@@ -86,9 +118,22 @@ class BoxManagementClass:
 
     def box_move(self, box: Union[Box, int], location: Union[Location, int]):
         """
-        Move an individual box in the inventory.
+        Move an individual box in the inventory.  The activity record for
+        this box will be changed to show  the new location.  The old
+        location will be dropped from the activity record.  If successful,
+        the box record will be returned.
 
-        See `:doc:Box Management<Box Management>` for intent and details.
+        Requirements:
+
+        *   Box is filled
+
+        *   Location is valid
+
+        Exceptions:
+
+            xxx - Cannot move an empty box
+
+            xxx - Location is not valid
 
         :param box:
         :param location:
@@ -109,9 +154,16 @@ class BoxManagementClass:
 
     def box_consume(self, box: Union[Box, int]):
         """
-        Consume (e.g. empty) a box.
+        Consume (e.g. empty) a box.  The box will be marked empty,
+        the activity record will be updated, and the box will be returned.
 
-        See `:doc:Box Management<Box Management>` for intent and details.
+        Requirements:
+
+        *   The box must not be empty when passed in.
+
+        Exception:
+
+            xxx - The box was already empty
 
         :param box:
         :return:
@@ -127,7 +179,19 @@ class BoxManagementClass:
 
     def pallet_finish(self, pallet: Union[Pallet, int]):
         """
-        Finish the processing of a pallet of boxes into inventory.
+        Finish the processing of a pallet  of boxes into inventory.  Each
+        box of the pallet will be processed.  Nothing will be returned.
+
+        Note - a pallet is still considered  valid even if there are no
+        boxes associated with it.
+
+        Requirements:
+
+        *   A valid pallet record or ID
+
+        Exception:
+
+            xxx - An invalid pallet record or ID was passed in
 
         :param pallet:
         :return:
