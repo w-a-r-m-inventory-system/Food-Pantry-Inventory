c2c2fa6c7f3836980194fbd6386a5511c2db9b85
John Cassidy <jocassid@gmail.com>
2020-06-10 00:46:50 -0400

    App permissions jocassid (#246)
    * WIP: started restricting access to certain pages.
    
    * WIP: created dummy_profile permission and made it required in most views
    
    dummy_profile is a dummy permission which not be granted to any user or group.  It effectively denies access to the views until appropriate permissions may be determined for the view.
    
    * WIP: Have permission checks in place for Volunteers
    
    * WIP: work on staff permissions
    
    * Staff permissions in place.
    
    * Fixed tests broken by restricting access to views.

--- fpiweb/tests/utility.py
+++ fpiweb/tests/utility.py
@@ -1,7 +1,8 @@
 
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission, User
 from django.test import Client
+from django.views import View
 
 from fpiweb.models import Profile
 
@@ -32,8 +33,44 @@ def create_user(first_name, last_name):
     return user
 
 
-def logged_in_user(first_name, last_name):
+def logged_in_user(first_name: str, last_name: str, view=None) -> Client:
     user = create_user(first_name, last_name)
+    if view is not None:
+        grant_required_permissions(user, view)
     client = Client()
     client.force_login(user)
     return client
+
+
+def grant_required_permissions(user:User, view:View) -> None:
+    permissions = getattr(view, 'permission_required', [])
+    for permission in permissions:
+        pieces = permission.split('.')
+        if len(pieces) != 2:
+            raise RuntimeError(f"Cannot split {permission} into 2 pieces")
+        app_label, codename = pieces
+
+        print(f"codename={codename}")
+        print(
+            Permission.objects.filter(
+                content_type__app_label=app_label,
+                content_type__model='pallet',
+            ).values_list(
+                'content_type__app_label',
+                'codename',
+            )
+        )
+
+        try:
+            permission = Permission.objects.get(
+                content_type__app_label__iexact=app_label,
+                codename__iexact=codename,
+            )
+        except Permission.DoesNotExist:
+            raise RuntimeError(
+                "Permission app_label={} codename={} not found".format(
+                    app_label,
+                    codename,
+                )
+            )
+        user.user_permissions.add(permission)
