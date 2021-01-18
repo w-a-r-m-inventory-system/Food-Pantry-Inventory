3721cc1859598210788bb3ff0ec115038618c4e3
Mike Rehner <babarehner@gmail.com>
2021-01-18 12:32:27 -0500

    Removed duplicate import debug_toolbar from urls.py

--- FPIDjango/urls.py
+++ FPIDjango/urls.py
@@ -21,17 +21,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from fpiweb.views import \
-    LoginView
-
-import debug_toolbar
+from fpiweb.views import LoginView
 
 
 __author__ = '(Multiple)'
 __project__ = "Food-Pantry-Inventory"
 __creation_date__ = "04/01/2019"
-
-
 
 
 
